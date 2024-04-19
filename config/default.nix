514b9ce5a4d176193fc75fb9f2e3e0371ac2e894
David Bellotti <dbellotti@gmail.com>
2024-04-19 15:30:27 -0700

    adds more plugins

--- config/default.nix
+++ config/default.nix
@@ -10,23 +10,40 @@
   #colorschemes.catppuccin.enable = true;
   #colorschemes.onedark.enable = true;
 
+  globals.mapleader = ",";
+
   opts = {
     number = true;
     relativenumber = true;
-    #shiftwidth = 2;
+    #shiftwidth = 2; # indenting needs testing
+    conceallevel = 2; # hide special characters, good for markdown
   };
 
   plugins = {
     #dashboard.enable = true;
-    fugitive.enable = true;
+    fugitive.enable = true; # git wrapper
     indent-blankline.enable = true;
     lualine.enable = true;
     nix.enable = true;
     treesitter.enable = true;
 
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<C-p>" = {
+          action = "git_files";
+          options = {
+            desc = "Telescope Git Files";
+          };
+        };
+        "<leader>ff" = "find_files";
+        "<leader>fg" = "live_grep";
+      };
+    };
+
     gitgutter = {
       enable = true;
-      #matchBackgrounds = true;
+      #matchBackgrounds = true; # not sure if this does anything
     };
   };
 }
