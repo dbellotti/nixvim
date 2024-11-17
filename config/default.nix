44de578c57026e32c28971cd92e55badd751c4c1
David Bellotti <dbellotti@gmail.com>
2024-11-17 12:36:40 -0800

    big update with plugins
    - lsp
    - cmp

--- config/default.nix
+++ config/default.nix
@@ -22,10 +22,24 @@
   plugins = {
     #dashboard.enable = true;
     fugitive.enable = true; # git wrapper
+    gitgutter.enable = true;
     indent-blankline.enable = true;
     lualine.enable = true;
-    nix.enable = true;
-    treesitter.enable = true;
+    nix.enable = true; # might remove if language server supports it and verified
+    #nvim-tree.enable = true;
+    #oil.enable = true;
+    web-devicons.enable = true;
+
+    # TODO
+    #  - conflicts with lsp semantic highlighting by linking to different
+    #  - highlight groups and drawing tokens from the language server
+    treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
+      };
+    };
 
     telescope = {
       enable = true;
@@ -44,9 +58,43 @@
       };
     };
 
-    gitgutter = {
+    lsp = {
       enable = true;
-      #matchBackgrounds = true; # not sure if this does anything
+      servers = {
+        nixd.enable = true;
+        pyright.enable = true;
+      };
+
+      # TODO figure out how to do code_actions
+      #keymaps.lspBuf = {
+      #  # TODO these appear to be working while commented out
+      #  "gd" = "definition";
+      #  "gD" = "references";
+      #  "gt" = "type_definition";
+      #  "gi" = "implementation";
+      #  "K" = "hover";
+      #  "<leader>ca" = "code_actions"; ???
+      #};
     };
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          { name = "nvim_lsp"; }
+          #{ name = "luasnip"; }
+          { name = "path"; }
+          { name = "buffer"; }
+        ];
+      };
+    };
+
+    # verify the following that seemed interesting
+    # not sure what each is doing (i'd like to demo)
+    #cmp-treesitter.enable = true;
+    #cmp-nvim-lsp.enable = true;
+    #cmp-spell.enable = true;
+    #cmp-dictionary.enable = true;
   };
 }
